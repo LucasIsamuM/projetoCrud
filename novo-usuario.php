3c154b6bcf50fd5687cb75bb0f4ab55f73b45400
LucasIsamuM <lucasisamu43@gmail.com>
2024-11-03 21:19:33 -0300

    Atualizaçao dia 03.11.2024

--- novo-usuario.php
+++ novo-usuario.php
@@ -1,5 +1,6 @@
 <h1>Novo Usuario </h1>
 <form action="?page=salvar" method="POST">
+    <input type="hidden" name="acao" value="cadastrar" >
     <div class = "mb-3">
         <label>Nome</label>
         <input type="text" name="nome" class= "form-control">
--- salvar-usuario.php
+++ salvar-usuario.php
@@ -1,12 +1,29 @@
 <?php
-switch ($variable) {
-    case 'value':
-        # code...
+switch ($_REQUEST["acao"]) {
+    case 'cadastrar':
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+        $data_nascimento = $_POST["data_nascimento"];
+
+        $sql = " INSERT INTO usuarios (nome , email , senha , data_nascimento ) VALUES ( '{nome}' , '{email}' , '{senha}' , '{data_nasc}')";
+
+        $res = $conn->query($sql);
+
+
+
+
+        
         break;
     
-    default:
-        # code...
-        break;
+    case 'editar':
+         
+         break;
+
+    case 'excluir':
+         
+         break;
+
 }
 
 ?>
