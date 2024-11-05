3031a5f443a7c10ebb88fdd2234538128124ef71
LucasIsamuM <lucasisamu43@gmail.com>
2024-11-04 22:54:03 -0300

    atualizacao dia 04.11.2024

--- salvar-usuario.php
+++ salvar-usuario.php
@@ -3,12 +3,26 @@ switch ($_REQUEST["acao"]) {
     case 'cadastrar':
         $nome = $_POST["nome"];
         $email = $_POST["email"];
-        $senha = $_POST["senha"];
+        $senha = md5($_POST["senha"]);
         $data_nascimento = $_POST["data_nascimento"];
 
-        $sql = " INSERT INTO usuarios (nome , email , senha , data_nascimento ) VALUES ( '{nome}' , '{email}' , '{senha}' , '{data_nasc}')";
+        $sql = " INSERT INTO usuarios (nome , email , senha , data_nascimento ) VALUES ( '{nome}' , '{email}' , '{senha}' , '{data_nascimento}')";
 
         $res = $conn->query($sql);
+
+        if($res==true){
+
+            print"<script>alert('cadastrado com sucesso');</script>";
+            print"<script>location.href='?page=listar';</script";
+
+
+        }else{
+
+            print"<script>alert('nao foi possivel cadastrar');</script>";
+            print"<script>location.href='?page=listar';</script";
+
+
+        }
 
 
 
